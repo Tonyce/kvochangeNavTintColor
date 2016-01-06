8cef43d83f387971bd102756919fdf29305e587f
D_ttang <D_ttang@163.com>
2016-01-06 15:57:03 +0800

    Initial Commit

---
+++ KVOLearn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KVOLearn
+//
+//  Created by D_ttang on 16/1/6.
+//  Copyright © 2016年 D_ttang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
